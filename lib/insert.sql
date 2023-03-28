f1a8f510ec2593e59da94863cd10029855b6a954
Mandy Petrakis <mandypetrakis@gmail.com>
2023-03-28 11:59:39 -0700

    commit tree 1eb2751f299c9696d6eb4dd4df480bc0fbaa6172

---
+++ lib/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Mr. Chocolate", 14, "M", "Brown", "Ravenous", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Rowdy", 14, "M", "Brown", "Rowdy", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Tabitha", 14, "F", "Brown", "Ravenous", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Sergeant Brown", 14, "F", "Brown", "Ravenous", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Melissa", 14, "M", "Brown", "Ravenous", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Grinch", 14, "M", "Brown", "Ravenous", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ("Wendy", 14, "M", "Brown", "Ravenous", 0);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 14, "M", "Brown", "Ravenous", 0);
+
+
+
+
+
+
